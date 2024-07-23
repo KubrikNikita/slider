c0f8e2b47ceb7bc31debf28f46edf26f2422cba1
nikita <kubrik2@mail.ru>
2024-07-23 09:32:47 +0500

    counter

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,15 +2,22 @@ import {useState} from "react";
 import './Slider.css'
 export const Slider = () => {
     const [value, setValue] = useState(0);
+    let counter = 0;
+    const increase = (value) => {
+        return setValue(value => value + 1);
+    }
+    const decrease = (value) => {
+        return setValue(value => value - 1);
+    }
     return(
         <div className={"slider"}>
             <button className={"buttons"} onClick={e => {
-                setValue(value + 1)
+                increase()
             }}>+
             </button>
             <div className={"count"}>{value}</div>
             <button className={"buttons"} onClick={e => {
-                setValue(value - 1)
+                decrease()
             }}>-
             </button>
         </div>
